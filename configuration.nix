02e0219763093512b57f1f53626f3f2baf5a2133
Zach Russell <zach@zachs.io>
2024-12-29 21:43:17 -0700

    clean up tty on login

--- configuration.nix
+++ configuration.nix
@@ -75,6 +75,17 @@ in
     };
   };
 
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
+
   # Add Hyprland
   programs.hyprland = {
     enable = true;
