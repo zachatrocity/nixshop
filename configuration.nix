73fa85796fd74850e6a7a550e1a1a167a78a6f06
Zach Russell <zach@zachs.io>
2024-12-29 21:41:25 -0700

    fixed hyprland command

--- configuration.nix
+++ configuration.nix
@@ -69,7 +69,7 @@ in
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --sessions ${pkgs.hyprland}/share/wayland-sessions";
         user = "greeter";
       };
     };
